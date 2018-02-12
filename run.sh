c09001b609d78ae4d50c8f899ffcb2b8ba071047
xichijun <xicj@wiseonline.com.cn>
2018-02-12 10:36:21 +0800

    更新脚本

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ CONFIG=$1
     CONFIG="./config.json"
 }
 
-./puwsgi run --target-app uwsgi_app.app --server-port 9999 --server-host 0.0.0.0 --config ${CONFIG}
+./puwsgi run --target-app mxproxy.app --server-port 9999 --server-host 0.0.0.0 --config ${CONFIG}
