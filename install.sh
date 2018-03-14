ed6a0e71d5709b29adfc18c36a3d13696917932e
xichijun <xicj@wiseonline.com.cn>
2018-03-14 11:49:54 +0800

    更新安装脚本

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 apt-get install build-essential python-dev python-pip
-UWSGI_PROFILE=gevent pip install uwsgi==2.0.14
+#UWSGI_PROFILE=gevent pip install uwsgi==2.0.14
+UWSGI_EMBED_PLUGINS=tornado,greenlet pip install tornado greenlet uwsgi==2.0.14
+
