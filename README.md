6ffd287dbda13d7f50c545fb52363f49f28dc33b
xichijun <xicj@wiseonline.com.cn>
2018-07-12 15:13:16 +0800

    ini 支持开发配置

--- README.md
+++ README.md
@@ -22,7 +22,10 @@ $VENV/bin/python setup.py develop
 
 * 启动服务 
 ```
+# 启动正式版本
 ./run_ini.sh
+# 启动开发版本
+./run_ini.sh -d
 ```
 
 ## Coding Started(如何提交代码)
--- run_ini.sh
+++ run_ini.sh
@@ -4,6 +4,10 @@ CONFIG=$1
 
 [ "$CONFIG"  == "" ] && {
     CONFIG="./production.ini"
+} || { 
+    [ "$CONFIG" == "-d" ] && {
+        CONFIG="./development.ini"
+    }
 }
 
 ./puwsgi run --server-port 6543 --server-host 0.0.0.0 --config ${CONFIG}
