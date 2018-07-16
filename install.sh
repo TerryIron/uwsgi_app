0646297cb188d2258473c6527cf9c60333a1a915
xichijun <xicj@wiseonline.com.cn>
2018-07-16 15:04:11 +0800

    更新安装脚本

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install build-essential python-dev python-pip
+apt-get install build-essential python-dev python-pip libmysqlclient-dev
 #UWSGI_PROFILE=gevent pip install uwsgi==2.0.14
-UWSGI_EMBED_PLUGINS=tornado,greenlet pip install tornado greenlet uwsgi==2.0.14
+UWSGI_EMBED_PLUGINS=tornado,greenlet pip install tornado==4.5.3 greenlet uwsgi==2.0.14
 
