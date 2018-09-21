b1a5392459071479841f0f5cb0423519e7036aee
xichijun <xicj@wiseonline.com.cn>
2018-09-21 14:43:29 +0800

    修复安装脚本

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 apt-get install build-essential python-dev python-pip libmysqlclient-dev
-#UWSGI_PROFILE=gevent pip install uwsgi==2.0.14
-UWSGI_EMBED_PLUGINS=tornado,greenlet pip install tornado==4.5.3 greenlet uwsgi==2.0.14
+virtualenv env --no-site-packages
+TORNADO=$(cat requirements.txt | grep "tornado=\|tornado$")
+[ "$TORNADO" == "" ] && {
+    UWSGI_EMBED_PLUGINS="greenlet"
+} || {
+    UWSGI_EMBED_PLUGINS="tornado,greenlet"
+}
+source env/bin/activate && pip install greenlet && cp -rf env/include/site/python2.7/greenlet /usr/local/include
+source env/bin/activate && UWSGI_EMBED_PLUGINS=$UWSGI_EMBED_PLUGINS pip install $TORNADO greenlet uwsgi==2.0.15
+source env/bin/activate && pip install -r requirements.txt
 
