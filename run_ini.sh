a6c7479675327263607ac014015440f2377b6288
xichijun <xicj@wiseonline.com.cn>
2018-11-01 15:48:20 +0800

    更新脚本

--- run_ini.sh
+++ run_ini.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+PWD=$(pwd)
 CONFIG=$1
 
 [ "$CONFIG"  == "" ] && {
-    CONFIG="./production.ini"
+    CONFIG="$PWD/production.ini"
 } || { 
     [ "$CONFIG" == "-d" ] && {
-        CONFIG="./development.ini"
+        CONFIG="$PWD/development.ini"
     }
 }
 
@@ -17,10 +18,12 @@ framework=$(cat $CONFIG | grep "application.framework")
     framework="flask"
 }
 
-for i in $(find | grep setup.py); do 
+for i in $(find | grep setup.py$); do 
     j=$(dirname $i); 
     [ "$j" != "." ] && {
         cd $j && python setup.py bdist_egg && cd -
     }
 done
+
+cd $PWD
 python puwsgi run --framework $framework --server-port 6543 --server-host 0.0.0.0 --config ${CONFIG}
