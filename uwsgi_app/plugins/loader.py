4e0c705d6db33108cbbc5fc729a2d5df01cd20a0
xichijun <xicj@wiseonline.com.cn>
2019-05-20 20:23:59 +0800

    代码优化

--- uwsgi_app/plugins/loader.py
+++ uwsgi_app/plugins/loader.py
@@ -17,33 +17,35 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+import collections
+
 
 __version__ = (0, 1, 0)
 
 
+class DictWrapper(collections.Mapping):
+
+   def __init__(self, **data):
+       self._data = data
+
+   def __setattr__(self, key, value):
+       if key == '_data':
+           if not hasattr(self, key):
+               return super(DictWrapper, self).__setattr__(key, value)
+       else:
+           return super(DictWrapper, self).__setattr__(key, value)
+
+   def __getitem__(self, key):
+       return self._data[key]
+
+   def __len__(self):
+       return len(self._data)
+
+   def __iter__(self):
+       return iter(self._data)
+
+
 class PluginLoader(object):
-    import collections
-
-    class DictWrapper(collections.Mapping):
-
-        def __init__(self, **data):
-            self._data = data
-
-        def __setattr__(self, key, value):
-            if key == '_data':
-                if not hasattr(self, key):
-                    return super(DictWrapper, self).__setattr__(key, value)
-            else:
-                return super(DictWrapper, self).__setattr__(key, value)
-
-        def __getitem__(self, key):
-            return self._data[key]
-
-        def __len__(self):
-            return len(self._data)
-
-        def __iter__(self):
-            return iter(self._data)
 
     @classmethod
     def get_logger(cls, name):
@@ -209,7 +211,7 @@ class PluginLoader(object):
         if _plugin and hasattr(_plugin, call) and callable(getattr(_plugin, call)):
             _call = getattr(_plugin, call)
             d = cls.Loader()
-            setattr(d, 'config', cls.DictWrapper(**config))
+            setattr(d, 'config', DictWrapper(**config))
             setattr(d, 'channel_scope', cls.result_channel)
             setattr(d, 'config_channel', cls.config_channel)
             setattr(d, 'current_channel', cls.result_channel[0])
@@ -239,7 +241,7 @@ class PluginLoader(object):
 
         setattr(cls.results, _new_name, dict())
         d = cls.Loader()
-        setattr(d, 'config', cls.DictWrapper(**getattr(cls.plugin_runner_config, _new_name)))
+        setattr(d, 'config', DictWrapper(**getattr(cls.plugin_runner_config, _new_name)))
         setattr(cls.plugin_loader, _new_name, d)
 
     @classmethod
